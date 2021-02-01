ce37c84eae14b7d297b15d81c911143c08b458e1
romanbatavi <romanbatavi98@gmail.com>
2021-02-01 22:52:17 +0700

    permision geolocation

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+function success(position){
+    console.log.(position);
+}
+
+function errpr(){
+    alert('Posisi Tidak Dapat Diakses')
+}
+function userlocation(){
+    if(navigator.geolocation){
+        alert('Geolocation Tidak Didukung');
+    } else
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+
 function index(){
     let app = document.getElementById('app');
     let h3 = document.createElement('h3');
     h3.innerHTML = 'Prayer Times';
 
     app.appendChild(h3);
+    userlocation();
 }
     index();
